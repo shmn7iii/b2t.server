9236fbbfdc8ff2c0a719c5b901c02ba547c26888
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-07-06 09:52:08 +0900

    update: add RPC methods

--- main.rb
+++ main.rb
@@ -1,4 +1,22 @@
 require 'sinatra'
+require 'json'
+require 'net/http'
+require 'bitcoin'
+require 'tapyrus'
+
+Bitcoin.chain_params = :signet
+Tapyrus.chain_params = :prod
+
+def bitcoinRPC
+  bitcoin_rpc_config = { schema: 'http', host: 'bitcoind', port: 38332, user: 'hoge', password: 'hoge' }
+  Bitcoin::RPC::BitcoinCoreClient.new(bitcoin_rpc_config)
+end
+
+def tapyrusRPC
+  tapyrus_rpc_config = { schema: 'http', host: 'tapyrusd', port: 2377, user: 'hoge', password: 'hoge' }
+  Tapyrus::RPC::TapyrusCoreClient.new(tapyrus_rpc_config)
+end
+
 configure do
   set :bind, '0.0.0.0'
 end
